dbd0a9db8e9d1476615a7c842dc32bc907cf10da
Manu-999 <manuelbalbas@gmail.com>
2020-05-23 10:57:20 +0200

    first route and dependencies installed

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const express_graphql = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const schema = buildSchema();
+const root = {
+  message: () => 'Hello World',
+};
+
+app.use(
+  '/graphql',
+  express_graphql({
+    schema: schema,
+    root: root,
+    graphiql: true,
+  })
+);
+
+app.listen(3000, () => console.log('Server up'));
